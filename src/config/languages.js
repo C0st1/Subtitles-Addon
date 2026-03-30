7f68d32307522833456d785a93981e6bac248c7c
C0st1 <87375320+C0st1@users.noreply.github.com>
2026-03-30 22:50:39 +0300

    Update languages.js

--- src/config/languages.js
+++ src/config/languages.js
@@ -1,15 +1,15 @@
 // src/config/languages.js
 
 const langMap = {
-  'eng': { opensubtitles: 'en', subdl: 'EN', subsource: 'english', subsro: 'en' },
-  'ron': { opensubtitles: 'ro', subdl: 'RO', subsource: 'romanian', subsro: 'ro' },
-  'fre': { opensubtitles: 'fr', subdl: 'FR', subsource: 'french', subsro: 'fra' },
-  'spa': { opensubtitles: 'es', subdl: 'ES', subsource: 'spanish', subsro: 'spa' },
-  'ger': { opensubtitles: 'de', subdl: 'DE', subsource: 'german', subsro: 'ger' },
-  'ita': { opensubtitles: 'it', subdl: 'IT', subsource: 'italian', subsro: 'ita' },
-  'hun': { opensubtitles: 'hu', subdl: 'HU', subsource: 'hungarian', subsro: 'ung' },
-  'por': { opensubtitles: 'pt', subdl: 'PT', subsource: 'portuguese', subsro: 'por' },
-  'gre': { opensubtitles: 'el', subdl: 'EL', subsource: 'greek', subsro: 'gre' }
+  'eng': { opensubtitles: 'en', subdl: 'EN', subsource: 'english' },
+  'ron': { opensubtitles: 'ro', subdl: 'RO', subsource: 'romanian' ,
+  'fre': { opensubtitles: 'fr', subdl: 'FR', subsource: 'french' },
+  'spa': { opensubtitles: 'es', subdl: 'ES', subsource: 'spanish' },
+  'ger': { opensubtitles: 'de', subdl: 'DE', subsource: 'german' },
+  'ita': { opensubtitles: 'it', subdl: 'IT', subsource: 'italian' },
+  'hun': { opensubtitles: 'hu', subdl: 'HU', subsource: 'hungarian' },
+  'por': { opensubtitles: 'pt', subdl: 'PT', subsource: 'portuguese' },
+  'gre': { opensubtitles: 'el', subdl: 'EL', subsource: 'greek' }
 };
 
 /**
