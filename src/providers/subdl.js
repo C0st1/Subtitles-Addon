6c3866d521c0aaf51bf2a0f9ddaac8928ad86ec7
C0st1 <87375320+C0st1@users.noreply.github.com>
2026-03-30 15:31:15 +0300

    Create subdl.js

---
+++ src/providers/subdl.js
@@ -0,0 +1,40 @@
+const { http } = require('../utils/http');
+const { toProviderCode, fromProviderCode } = require('../config/languages');
+
+module.exports = async (params) => {
+  const { imdbIdFull, season, episode, type, languages, config } = params;
+  const apiKey = config.subdl_api_key || process.env.SUBDL_API_KEY;
+  if (!apiKey) return[];
+
+  const subdlLangs = languages.map(l => toProviderCode(l, 'subdl')).filter(Boolean).join(',');
+  if (!subdlLangs) return[];
+
+  const query = new URLSearchParams({
+    api_key: apiKey,
+    imdb_id: imdbIdFull,
+    type: type === 'series' ? 'tv' : 'movie',
+    languages: subdlLangs
+  });
+
+  if (type === 'series' && season && episode) {
+    query.append('season_number', season);
+    query.append('episode_number', episode);
+  }
+
+  const res = await http.get(`https://api.subdl.com/api/v1/subtitles?${query.toString()}`);
+  
+  const results = [];
+  for (const sub of res.data.subtitles ||[]) {
+    const isoLang = fromProviderCode(sub.language, 'subdl');
+    if (!isoLang) continue;
+
+    const payload = Buffer.from(JSON.stringify({ url: sub.url })).toString('base64url');
+    results.push({
+      id: payload,
+      lang: isoLang,
+      provider: 'subdl',
+      releaseName: sub.release_name
+    });
+  }
+  return results;
+};
